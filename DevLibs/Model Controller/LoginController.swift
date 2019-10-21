427674ea7f33545ea266a6cfba5aa69ef52e0fed
Alexandra Rhodes <anrhodes5738@gmail.com>
2019-10-21 11:34:58 -0500

    Initial Commit + XCode Project

---
+++ DevLibs/Model Controller/LoginController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginController.swift
+//  DevLibs
+//
+//  Created by Alex Rhodes on 10/21/19.
+//  Copyright © 2019 Alex Rhodes. All rights reserved.
+//
+
+import Foundation
+
+class LoginController {
+    
+    
+    func login() {
+        
+    }
+    
+    func register() {
+        
+    }
+    
+}
