94a3f44be7933807f0a182de76a01e39df205a07
AustinPotts <pottsevolvements@gmail.com>
2019-10-22 16:46:53 -0400

    Configured Core Data + Networking

--- DevLibs/Model Controller/LoginController.swift
+++ DevLibs/Model Controller/LoginController.swift
@@ -7,16 +7,146 @@
 //
 
 import Foundation
+import UIKit
+
+enum HTTPMethod: String{
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+enum NetworkError: Error{
+    case ecodingError
+    case responseError
+    case otherError(Error)
+    case noData
+    case noDecode
+    case noToken
+}
+
+enum HeaderNames: String {
+    case authoriaztion = "Authorization"
+    case contentType = "json"
+}
 
 class LoginController {
     
     
-    func login() {
+    var bearer: Bearer?
+    
+    //MARK: Base API URL
+    private let baseUrl = URL(string: "https://dev-libs-test.herokuapp.com")!
+    
+    
+    
+    //MARK: - Register the User (POST)
+    func signUp(with user: UserRepresentation, completion: @escaping(NetworkError?)-> Void){
+        
+        //Build the URL
+        let requestURL = baseUrl.appendingPathComponent("api")
+                                .appendingPathComponent("auth")
+                                .appendingPathComponent("register")
+        
+        //Build the request
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        //Turn request into JSON
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        //Perform the request
+        let encoder = JSONEncoder()
+        
+        do {
+           let userJSON = try encoder.encode(user)
+            request.httpBody = userJSON
+        } catch {
+            NSLog("Error encoding data: \(error)")
+            completion(.ecodingError)
+            return
+            
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                print(response.statusCode)
+                completion(.responseError)
+                return
+            }
+            
+            if let error = error {
+                NSLog("Error Creating user on server: \(error)")
+                completion(.otherError(error))
+                return
+            }
+            completion(nil)
+        }.resume()
         
     }
     
-    func register() {
+    
+    
+    //MARK: - Login the User (POST)
+    func signIn(with user: UserRepresentation, completion: @escaping(NetworkError?)-> Void){
         
+        //Build Url
+        let loginURL = baseUrl.appendingPathComponent("api")
+                              .appendingPathComponent("auth")
+                              .appendingPathComponent("login")
+        
+        //Build request
+        var request = URLRequest(url: loginURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let encoder = JSONEncoder()
+        
+        do {
+            request.httpBody = try encoder.encode(user)
+        } catch {
+            NSLog("Error: \(error)")
+            completion(.ecodingError)
+            return
+        }
+        
+        //Perform the request
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200{
+                completion(.noData)
+                return
+            }
+            
+            if let error = error {
+                NSLog("Error fetching data tasks: \(error)")
+                completion(.otherError(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.noData)
+                return
+            }
+            
+            do {
+                let bearer = try JSONDecoder().decode(Bearer.self, from: data)
+                
+                self.bearer = bearer
+            } catch {
+                completion(.noData)
+                return
+                
+            }
+            
+            completion(nil)
+        }.resume()
     }
+    
+    
+   
     
 }
--- DevLibs/View Cotrollers/LoginViewController.swift
+++ DevLibs/View Cotrollers/LoginViewController.swift
@@ -16,6 +16,53 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    var loginController: LoginController?
+    
+    
+    //MARK: Button Tapped Outlet Action
+    
+//    guard let username = usernameTextField.text,
+//               let password = passwordTextField.text,
+//                 !username.isEmpty,
+//                  !password.isEmpty else{return}
+//
+//           let user = User(username: username, password: password)
+    
+    
+    
+    
+    //MARK: ViewController SignUp + Sign In
+    func signUp(with user: UserRepresentation) {
+           loginController?.signUp(with: user, completion: { (error) in
+               if let error = error{
+                   NSLog("Error signing up \(error)")
+               } else {
+                   let alert = UIAlertController(title: "Sign Up Success", message: "Sign in now please", preferredStyle: .alert)
+                   
+                   let okAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+                   
+                   alert.addAction(okAction)
+                   
+                   
+               }
+               
+           })
+           
+       }
+       
+       func signIn(with user: UserRepresentation){
+           loginController?.signIn(with: user, completion: { (error) in
+               if let error = error {
+                   NSLog("Error\(error)")
+                   
+               } else {
+                   DispatchQueue.main.async {
+                       self.dismiss(animated: true, completion: nil)
+                   }
+               }
+           })
+       }
+       
 
     /*
     // MARK: - Navigation
